11f31bdd4130febe48e52b2f8b55d495eee8130a
ptedd <parkteddy2845@gmail.com>
2020-08-24 17:59:38 -0500

    board setup + drawing

--- main.lua
+++ main.lua
@@ -1,3 +1,205 @@
+-- https://simplegametutorials.github.io/blocks/
+
+function love.load()
+    love.graphics.setBackgroundColor(255, 255, 255)
+    
+    gridXCount = 10
+    gridYCount = 18
+    
+    -- inert blocks
+    inert = {}
+    for y = 1, gridYCount do
+        inert[y] = {}
+        for x = 1, gridXCount do
+            inert[y][x] = ' '
+        end
+    end
+    
+    -- piece structures
+    pieceStructures = {
+        {
+            {
+                {' ', ' ', ' ', ' '},
+                {'i', 'i', 'i', 'i'},
+                {' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' '},
+            },
+            {
+                {' ', 'i', ' ', ' '},
+                {' ', 'i', ' ', ' '},
+                {' ', 'i', ' ', ' '},
+                {' ', 'i', ' ', ' '},
+            },
+        },
+        {
+            {
+                {' ', ' ', ' ', ' '},
+                {' ', 'o', 'o', ' '},
+                {' ', 'o', 'o', ' '},
+                {' ', ' ', ' ', ' '},
+            },
+        },
+        {
+            {
+                {' ', ' ', ' ', ' '},
+                {'j', 'j', 'j', ' '},
+                {' ', ' ', 'j', ' '},
+                {' ', ' ', ' ', ' '},
+            },
+            {
+                {' ', 'j', ' ', ' '},
+                {' ', 'j', ' ', ' '},
+                {'j', 'j', ' ', ' '},
+                {' ', ' ', ' ', ' '},
+            },
+            {
+                {'j', ' ', ' ', ' '},
+                {'j', 'j', 'j', ' '},
+                {' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' '},
+            },
+            {
+                {' ', 'j', 'j', ' '},
+                {' ', 'j', ' ', ' '},
+                {' ', 'j', ' ', ' '},
+                {' ', ' ', ' ', ' '},
+            },
+        },
+        {
+            {
+                {' ', ' ', ' ', ' '},
+                {'l', 'l', 'l', ' '},
+                {'l', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' '},
+            },
+            {
+                {' ', 'l', ' ', ' '},
+                {' ', 'l', ' ', ' '},
+                {' ', 'l', 'l', ' '},
+                {' ', ' ', ' ', ' '},
+            },
+            {
+                {' ', ' ', 'l', ' '},
+                {'l', 'l', 'l', ' '},
+                {' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' '},
+            },
+            {
+                {'l', 'l', ' ', ' '},
+                {' ', 'l', ' ', ' '},
+                {' ', 'l', ' ', ' '},
+                {' ', ' ', ' ', ' '},
+            },
+        },
+        {
+            {
+                {' ', ' ', ' ', ' '},
+                {'t', 't', 't', ' '},
+                {' ', 't', ' ', ' '},
+                {' ', ' ', ' ', ' '},
+            },
+            {
+                {' ', 't', ' ', ' '},
+                {' ', 't', 't', ' '},
+                {' ', 't', ' ', ' '},
+                {' ', ' ', ' ', ' '},
+            },
+            {
+                {' ', 't', ' ', ' '},
+                {'t', 't', 't', ' '},
+                {' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' '},
+            },
+            {
+                {' ', 't', ' ', ' '},
+                {'t', 't', ' ', ' '},
+                {' ', 't', ' ', ' '},
+                {' ', ' ', ' ', ' '},
+            },
+        },
+        {
+            {
+                {' ', ' ', ' ', ' '},
+                {' ', 's', 's', ' '},
+                {'s', 's', ' ', ' '},
+                {' ', ' ', ' ', ' '},
+            },
+            {
+                {'s', ' ', ' ', ' '},
+                {'s', 's', ' ', ' '},
+                {' ', 's', ' ', ' '},
+                {' ', ' ', ' ', ' '},
+            },
+        },
+        {
+            {
+                {' ', ' ', ' ', ' '},
+                {'z', 'z', ' ', ' '},
+                {' ', 'z', 'z', ' '},
+                {' ', ' ', ' ', ' '},
+            },
+            {
+                {' ', 'z', ' ', ' '},
+                {'z', 'z', ' ', ' '},
+                {'z', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' '},
+            },
+        },
+    }
+
+    -- current falling piece
+    pieceType = 1
+    pieceRotation = 1
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 350, 300)
+    
+    -- block drawing function
+    local function drawBlock(block, x, y)
+        
+        -- define colors for each block type
+        local colors = {
+            [' '] = {.87, .87, .87},
+            i = {.47, .76, .94},
+            j = {.93, .91, .42},
+            l = {.49, .85, .76},
+            o = {.92, .69, .47},
+            s = {.83, .54, .93},
+            t = {.97, .58, .77},
+            z = {.66, .83, .46},
+        }
+        
+        -- color of block
+        local color = colors[block]
+        
+        -- draw block
+        love.graphics.setColor(color)
+        local blockSize = 20
+        local blockDrawSize = blockSize - 1
+        love.graphics.rectangle (
+            'fill',
+            (x-1) * blockSize,
+            (y-1) * blockSize,
+            blockDrawSize,
+            blockDrawSize
+        )
+    end
+    
+    -- draw grid
+    for y = 1, gridYCount do
+        for x = 1, gridXCount do
+            drawBlock(inert[y][x], x, y)
+        end
+    end
+    
+    -- draw falling piece
+    for y = 1, 4 do
+        for x = 1, 4 do
+            local block = pieceStructures[pieceType][pieceRotation][y][x]
+            if block ~= ' ' then
+                drawBlock(block, x, y)
+            end
+        end
+    end
+    
 end
